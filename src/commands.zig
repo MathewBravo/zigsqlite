1fdaccfd6a0b373305a84932a0a71659bce39c4c
MathewBravo <mathewbravo94@gmail.com>
2025-01-21 17:17:09 -0500

    reworked some objects

--- src/commands.zig
+++ src/commands.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
-const MetaCommandResult = enum { META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND };
+const MetaCommandResult = enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND,
+};
 
 const PrepareResult = enum {
     PREPARE_SUCCESS,
@@ -17,16 +20,12 @@ const Statement = struct {
     type: StatementType,
 };
 
-pub fn command_handler(stdout: anytype, user_input: []u8) !void {
+pub fn command_handler(user_input: []u8) !void {
     var met_command_res: MetaCommandResult = undefined;
     if (user_input.len > 0 and user_input[0] == '.') {
         met_command_res = execute_meta_command(user_input);
     } else {
         met_command_res = MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND;
-    }
-
-    if (met_command_res == MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND) {
-        try stdout.print("Unknown Meta Command: {s}\n", .{user_input});
     }
 
     var statement = Statement{
@@ -50,12 +49,12 @@ fn execute_meta_command(command: []u8) MetaCommandResult {
 }
 
 fn prepare_statement(user_input: []u8, statement: *Statement) PrepareResult {
-    if (std.mem.eql(u8, user_input, "insert")) {
+    if (std.mem.eql(u8, user_input[0..6], "insert")) {
         statement.type = StatementType.STATEMENT_INSERT;
         return PrepareResult.PREPARE_SUCCESS;
     }
 
-    if (std.mem.eql(u8, user_input, "select")) {
+    if (std.mem.eql(u8, user_input[0..6], "select")) {
         statement.type = StatementType.STATEMENT_SELECT;
         return PrepareResult.PREPARE_SUCCESS;
     }
@@ -76,4 +75,3 @@ fn execute_statement(statement: *Statement) void {
         },
     }
 }
-
--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,13 @@ const commands = @import("commands.zig");
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
 
     while (true) {
         var buffer: [1024]u8 = undefined;
         print_prompt();
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-        try commands.command_handler(stdout, user_input);
+        try commands.command_handler(user_input);
     }
 }
 
